73b815d3d591a4289fb75f1837e001d792e2bf60
YassineOH <yassine.yas98@gmail.com>
2023-06-20 16:36:16 +0100

    trigger rebuild

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import xss from "xss-clean";
-import helmet from "helmet";
-import * as dotenv from "dotenv";
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import xss from 'xss-clean';
+import helmet from 'helmet';
+import * as dotenv from 'dotenv';
 dotenv.config();
-// ADD A COMMENT
+// ADD A COMMENT d
 
-import solarRouter from "./routes/solarRoutes.js";
-import clientRequestRouter from "./routes/requestRoutes.js";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
+import solarRouter from './routes/solarRoutes.js';
+import clientRequestRouter from './routes/requestRoutes.js';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 app.use(express.json());
@@ -18,16 +18,16 @@ app.use(
   helmet({
     contentSecurityPolicy: {
       directives: {
-        "img-src": ["'self'", "https: data:"],
-        "script-src": [
+        'img-src': ["'self'", 'https: data:'],
+        'script-src': [
           "'self'",
-          "https://maps.googleapis.com",
-          "https://maps.gstatic.com",
+          'https://maps.googleapis.com',
+          'https://maps.gstatic.com',
         ],
-        "default-src": [
+        'default-src': [
           "'self'",
-          "https://maps.googleapis.com",
-          "https://maps.gstatic.com",
+          'https://maps.googleapis.com',
+          'https://maps.gstatic.com',
         ],
       },
     },
@@ -38,16 +38,16 @@ app.use(
 app.use(xss());
 app.use(cors());
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(path.resolve(__dirname, "../client/dist")));
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
-app.use("/api/v1", solarRouter);
-app.use("/api/v1", clientRequestRouter);
+app.use('/api/v1', solarRouter);
+app.use('/api/v1', clientRequestRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
 });
 
 const port = process.env.PORT || 5000;
